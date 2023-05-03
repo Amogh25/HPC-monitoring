7aea33a2e5f710a6c724c19efab364064548f740
BrindaS11 <85933206+BrindaS11@users.noreply.github.com>
2023-05-03 17:59:06 +0530

    Update kafka_metrics_generation.py

--- Kafka/kafka_metrics_generation.py
+++ Kafka/kafka_metrics_generation.py
@@ -2,6 +2,14 @@ from kafka import KafkaProducer, KafkaConsumer
 import json
 from time import sleep
 import random
+import argparse
+
+# Parse the command line arguments
+parser = argparse.ArgumentParser(description='Simulate metrics data and send to Kafka topic.')
+parser.add_argument('file', type=str, help='Name of the file containing the list of monitored metrics')
+args = parser.parse_args()
+# Extract the topic name from the file name
+topic_name = args.file.split('.')[0]
 
 # Define Kafka producer properties
 producer = KafkaProducer(
@@ -10,25 +18,14 @@ producer = KafkaProducer(
 )
 
 # Define a list of monitored metrics
-with open("pcm-alert-kafka.txt", "r") as f:
+with open(args.file, "r") as f:
     metrics_list = [json.loads(line) for line in f]
+
+with open('topiclist.txt', 'a') as f:
+    f.write(topic_name + '\n')
 
 # Define a function to generate random metrics data
 length=len(metrics_list)
-
-
-def size_simulation():
-    i=-1
-    
-    time_delay = 5.0
-    while True:
-        i=(i+1)%length
-        metrics_data = metrics_list[i]
-        if random.uniform(1, 10) <= 3:
-            metrics_data *= 15
-        producer.send('tests2', value=metrics_data)
-        producer.flush()
-        sleep(time_delay)
 
 def frequency_simulation():
     i=-1   
@@ -38,7 +35,7 @@ def frequency_simulation():
         metrics_data = metrics_list[i]
         if random.uniform(1, 10) <= 3:
             time_delay = 1
-        producer.send('tests2', value=metrics_data)
+        producer.send(topic_name, value=metrics_data)
         producer.flush()
         sleep(time_delay)
 
@@ -50,16 +47,14 @@ def latency_simulation():
         metrics_data = metrics_list[i]
         if random.uniform(1, 10) <= 3:
             sleep(5)
-        producer.send('tests2', value=metrics_data)
+        producer.send(topic_name, value=metrics_data)
         producer.flush()
         sleep(time_delay)
 
 # Choose simulation
-choices = [1, 2, 3]
+choices = [1, 2]
 choice = random.choice(choices)
 if choice == 1:
-    size_simulation()
-elif choice == 2:
     latency_simulation()
-elif choice == 3:
+elif choice == 2:
     frequency_simulation()
