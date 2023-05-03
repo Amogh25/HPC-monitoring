366360be50f6d4a28558d93d6580b266540601b5
sakshyapoovamma <97402138+sakshyapoovamma@users.noreply.github.com>
2023-05-03 18:04:04 +0530

    Update and rename test_kafka_alerting.py to testing_metrics.py

--- Selenium/testing_metrics.py
+++ Selenium/testing_metrics.py
@@ -2,6 +2,7 @@ import json
 import time
 import pandas as pd
 import os
+import argparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -16,12 +17,17 @@ from colorama import Fore,Style
 
 
 class TestKafka():
+  metrics = ['PDU_Power','Sensors_Node']
   def setup_method(self):
     options=webdriver.ChromeOptions()
     options.add_argument("headless")
-    path = "/home/sasha/projects/Selenium/panel-data"
-    prefs = {"download.default_directory" : path}
-    options.add_experimental_option("prefs",prefs)
+    path = "panel-data/"
+    options.add_experimental_option('prefs', {
+    'download.default_directory': path,
+    'download.prompt_for_download': False,
+    'download.directory_upgrade': True,
+    'safebrowsing.enabled': True
+})
     self.driver = webdriver.Chrome(service=Service('/usr/local/bin/chromedriver'),options=options)
     self.vars = {}
   
@@ -41,35 +47,33 @@ class TestKafka():
 
     title_list=[]
     graph_list=[]
-    metrics = ['CPU Usage','Messages In Per Topic','Bytes In Per Topic','Bytes Out Per Topic']
-    dashboard_code="8pLvf9BVk"
+    dashboard_code="GYYoBhsVk"
     dashboard_name = "kafka-overview"
     time_interval = "5m"
+    refresh_time = "5s"
     for panel in data['panels']:
-      if panel['title'] in metrics:
-        id = str(panel['id'])
-        title_list.append(panel['title'])
-        url="http://localhost:3000/d/"+dashboard_code+"/"+ dashboard_name +"?orgId=1&editPanel="+id+"&from=now-"+time_interval+"&to=now&inspect="+id
-        graph_list.append(url)
+      id = str(panel['id'])
+      title_list.append(panel['title'])
+      url="http://localhost:3000/d/"+dashboard_code+"/"+ dashboard_name +"?orgId=1&editPanel="+id+"&from=now-"+time_interval+"&to=now&refresh="+refresh_time+"&inspect="+id
+      graph_list.append(url)
 
       if panel.get('panels',0) != 0:
         for pan in panel['panels']:
-            if pan['title'] in metrics:
-                id = str(pan['id'])
-                title_list.append(pan['title'])
-                url="http://localhost:3000/d/"+dashboard_code+"/"+ dashboard_name +"?orgId=1&editPanel="+id+"&from=now-"+time_interval+"&to=now&inspect="+id
-                graph_list.append(url)
+            id = str(pan['id'])
+            title_list.append(pan['title'])
+            url="http://localhost:3000/d/"+dashboard_code+"/"+ dashboard_name +"?orgId=1&editPanel="+id+"&from=now-"+time_interval+"&to=now&refresh="+refresh_time+"&inspect="+id
+            graph_list.append(url)
 
     print("Panels found")
     for i in range(len(graph_list)):
       print(title_list[i])
       print(graph_list[i])
-    return graph_list
+    return graph_list,title_list
 
 
   def test_login(self,url1):
-    usr="username"
-    pw="password"
+    usr="admin"
+    pw="Rockwood0542@"
     self.driver.get(url1)
     self.driver.set_window_size(1292, 638)
     self.driver.execute_script("window.scrollTo(0,0)")
@@ -89,8 +93,6 @@ class TestKafka():
     actions = ActionChains(self.driver)
     actions.move_to_element(element).perform()
     self.driver.find_element(By.XPATH, "/html/body/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/div/div[1]/div/div/div[1]/div[1]/div/div").click()
-    
-    
     self.driver.execute_script("window.scrollTo(0,0)")
     time.sleep(3)
     try:
@@ -118,70 +120,78 @@ class TestKafka():
       self.driver.find_element(By.XPATH, "/html/body/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/div/div[1]/button/span").click()
       time.sleep(3)
 
-  def test_alerting(self):
+  def acceptance(self,title):
 
-    directory = "/home/sasha/projects/Selenium/panel-data/"
-    metrics = ['CPU Usage','Messages In Per Topic','Bytes In Per Topic','Bytes Out Per Topic']
-    metrics_dict = dict()
-    for i in range(len(metrics)):
-       metrics_dict[metrics[i]]= [f for f in os.listdir(directory) if f.startswith(metrics[i]) and f.endswith(".csv")]
-       print(metrics_dict[metrics[i]])
+    filepath = "metrics/config1.json"
+    with open(filepath,"r") as f:
+      file = json.load(f)
+      data = dict()
+      data['title'] = title
+      data['topic'] = title.lower()
+      data['threshold'] = int(input("Enter acceptance criteria for " + title +" :"))
+      file['panels'].append(data)
+    with open(filepath,"w") as f:
+      json.dump(file,f)
 
+  def test_alerting(self,title):
+    with open("metrics/config1.json","r") as f:
+      accept = json.load(f)
+
+    for panel in accept['panels']:
+      if panel['title'] == title:
+        threshold = panel['threshold']
+        topic = panel['topic']
     
-
-    #Alerting message size
     directory = "panel-data/"
-    data_file = os.path.join(directory,metrics_dict['Bytes In Per Topic'][0])
+    files = [f for f in os.listdir(directory) if f.startswith(title) and f.endswith(".csv")]
+    latest_file = max(files, key=lambda x: os.path.getmtime(os.path.join(directory, x)))
+    data_file = os.path.join(directory,latest_file)
     data = pd.read_csv(data_file)
-    print(Fore.RED , "ALERT: Message size")
-    fail_tests = data[data.tests2 > 40]
-    print(Fore.RED,fail_tests[['Time','tests2']])
-    print("")
-
-    #Alerting high latency
-    directory = "panel-data/"
-    data_file = os.path.join(directory,metrics_dict['Messages In Per Topic'][0])
-    data = pd.read_csv(data_file)
-    print(Fore.RED , "ALERT: High Latency")
-    fail_tests = data[data.tests2 < 0.150]
-    print(Fore.RED,fail_tests[['Time','tests2']])
-    print("")
-
-    #Alerting high freguency
-    directory = "panel-data/"
-    data_file = os.path.join(directory,metrics_dict['Messages In Per Topic'][0])
-    data = pd.read_csv(data_file)
-    print(Fore.RED , "ALERT: High Frequency")
-    fail_tests = data[data.tests2 > 0.500]
-    print(Fore.RED,fail_tests[['Time','tests2']])
-    print("")
+    print(Fore.RED , "ALERT:")
+    fail_tests = data.loc[data[topic] < threshold]
+    print(Fore.RED,fail_tests[['Time',topic]])
     print(Style.RESET_ALL)
+    print("")
+    directory = "alert/"
+    file_name = topic +".csv"
+    file_path = os.path.join(directory,file_name)
+    fail_tests.to_csv(file_path,index=False)
 
     
-if __name__=="__main__":
+    
+if __name__ =="__main__":
 
   test1 = TestKafka()
   url1="http://localhost:3000/"
   test1.setup_method()
   test1.test_login(url1)
   print("Login Successful")
-  list = test1.url_generator()
+  graph_list,title_list = test1.url_generator()
   j=1
-  for i in list:
-    print("Fetching file "+str(j))
-    start = time.time()
-    try:
-      test1.test_fetchpaneldata(i)
-    except NoSuchElementException:
-      print(Fore.RED , "Alert: No Data")
-      print(Style.RESET_ALL)
-    else:
-      end = time.time()
-      print("File "+str(j)+" download complete")
-      print(f"Time taken : {(end - start):.03f}s")
-    finally:
-      j+=1
+  for i in range(len(graph_list)):
+    if title_list[i] in test1.metrics:
+      print("Fetching file "+str(j))
+      start = time.time()
+      try:
+        test1.test_fetchpaneldata(graph_list[i])
+      except NoSuchElementException:
+        print(Fore.RED , "Alert: No Data")
+        print(Style.RESET_ALL)
+      else:
+        end = time.time()
+        print("File "+str(j)+" download complete")
+        print(f"Time taken : {(end - start):.03f}s")
+        j=j+1
+  with open("metrics/config1.json","w") as f:
+    accept = dict()
+    accept['panels'] = []
+    json.dump(accept,f)
+  for i in range(len(graph_list)):
+    if title_list[i] in test1.metrics:
+        test1.acceptance(title_list[i])
+        test1.test_alerting(title_list[i])
+    
   test1.teardown_method()
-  test1.test_alerting()
+  
   
 
