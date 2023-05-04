20be5aadc28320285d6e0034d268a884135e5f71
BrindaS11 <85933206+BrindaS11@users.noreply.github.com>
2023-05-04 14:51:58 +0530

    Update kafka_metrics_generation.py

--- Kafka/kafka_metrics_generation.py
+++ Kafka/kafka_metrics_generation.py
@@ -3,6 +3,9 @@ import json
 from time import sleep
 import random
 import argparse
+import tempfile
+import atexit
+import os
 
 # Parse the command line arguments
 parser = argparse.ArgumentParser(description='Simulate metrics data and send to Kafka topic.')
@@ -21,17 +24,28 @@ producer = KafkaProducer(
 with open(args.file, "r") as f:
     metrics_list = [json.loads(line) for line in f]
 
-with open('topiclist.txt', 'a') as f:
+# Create a temporary file to write the topic name
+with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+    temp_file_path = f.name
     f.write(topic_name + '\n')
+    print("Temporary file created: ", temp_file_path)
+
+    # Register a function to delete the temporary file
+    atexit.register(lambda: os.remove(temp_file_path))
+
+# Read the content of the temporary file
+with open(temp_file_path, 'r') as f:
+    file_content = f.read()
+    print("Temporary file content: ", file_content)
 
 # Define a function to generate random metrics data
-length=len(metrics_list)
+length = len(metrics_list)
 
 def frequency_simulation():
-    i=-1   
+    i = -1   
     time_delay = 5.0
     while True:
-        i=(i+1)%length
+        i = (i + 1) % length
         metrics_data = metrics_list[i]
         if random.uniform(1, 10) <= 3:
             time_delay = 1
@@ -40,10 +54,10 @@ def frequency_simulation():
         sleep(time_delay)
 
 def latency_simulation():
-    i=-1
+    i = -1
     time_delay = 15.0
     while True:
-        i=(i+1)%length
+        i = (i + 1) % length
         metrics_data = metrics_list[i]
         if random.uniform(1, 10) <= 3:
             sleep(5)
@@ -58,3 +72,8 @@ if choice == 1:
     latency_simulation()
 elif choice == 2:
     frequency_simulation()
+
+
+# Delete the temporary file after the code execution
+import os
+os.remove(temp_file_path)
